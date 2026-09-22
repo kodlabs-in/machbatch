56b8534c0bbbfaebcb3ee2e8840dc0c945f398d1
Prince <prince@kodlabs.in>
2026-09-22 17:49:30 +0530

    chore: prepare v0.1.0 release

--- Sources/Compatibility/CompatibilityProfile.swift
+++ Sources/Compatibility/CompatibilityProfile.swift
@@ -3,7 +3,7 @@ import ClusterCore
 public enum CompatibilityProfile {
   public static let current = ProductIdentity(
     name: "MachBatch",
-    releaseVersion: "0.0.0-dev",
+    releaseVersion: "0.1.0",
     cliCompatibility: "Slurm 26.05.4 CLI-compatible"
   )
 }
